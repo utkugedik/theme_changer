4a0ecc2064190f285bf4ada20349a705383bf14e
utkugedik <info.utkugedik@gmail.com>
2021-01-18 03:08:52 +0300

    add adaptive theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,38 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'package:dynamic_theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final savedThemeMode = await AdaptiveTheme.getThemeMode();
+  runApp(MyApp(savedThemeMode: savedThemeMode));
+}
 
 class MyApp extends StatelessWidget {
+  final AdaptiveThemeMode savedThemeMode;
+
+  const MyApp({Key key, this.savedThemeMode}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+    return AdaptiveTheme(
+      light: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.green,
+        accentColor: Colors.amber,
+      ),
+      dark: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.red,
+        accentColor: Colors.amber,
+      ),
+      initial: savedThemeMode ?? AdaptiveThemeMode.light,
+      builder: (theme, darkTheme) => MaterialApp(
+        theme: theme,
+        darkTheme: darkTheme,
+        home: MyHomePage(),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
